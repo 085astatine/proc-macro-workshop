0ca7351a75059624cf2103833eaa8be57bd78330
085astatine <astatine085@gmail.com>
2022-01-21 22:20:01 +0900

    builder: 03-call-setters

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -7,13 +7,19 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = syn::parse_macro_input!(input as syn::DeriveInput);
 
     let struct_builder = struct_builder(&input);
+    let impl_struct = impl_struct(&input);
     let impl_builder = impl_builder(&input);
 
     let expanded = quote::quote! {
         #struct_builder
+        #impl_struct
         #impl_builder
     };
     proc_macro::TokenStream::from(expanded)
+}
+
+fn builder_name(data: &syn::DeriveInput) -> syn::Ident {
+    quote::format_ident!("{}Builder", data.ident)
 }
 
 fn struct_fields(data: &syn::Data) -> Vec<&syn::Field> {
@@ -27,7 +33,7 @@ fn struct_fields(data: &syn::Data) -> Vec<&syn::Field> {
 }
 
 fn struct_builder(input: &syn::DeriveInput) -> proc_macro2::TokenStream {
-    let struct_name = quote::format_ident!("{}Builder", input.ident);
+    let struct_name = builder_name(input);
     let visibility = &input.vis;
     let struct_fields = struct_fields(&input.data);
     let fields = struct_fields
@@ -48,13 +54,13 @@ fn struct_builder_field(field: &syn::Field) -> proc_macro2::TokenStream {
     }
 }
 
-fn impl_builder(input: &syn::DeriveInput) -> proc_macro2::TokenStream {
+fn impl_struct(input: &syn::DeriveInput) -> proc_macro2::TokenStream {
     let name = &input.ident;
-    let builder_name = quote::format_ident!("{}Builder", input.ident);
+    let builder_name = builder_name(input);
     let struct_fields = struct_fields(&input.data);
     let builder_initialize_fields = struct_fields
         .iter()
-        .map(|field| impl_builder_initialize_field(field));
+        .map(|field| impl_struct_initialize_builder_field(field));
     quote::quote! {
         impl #name {
             pub fn builder() -> #builder_name {
@@ -66,9 +72,31 @@ fn impl_builder(input: &syn::DeriveInput) -> proc_macro2::TokenStream {
     }
 }
 
-fn impl_builder_initialize_field(field: &syn::Field) -> proc_macro2::TokenStream {
+fn impl_struct_initialize_builder_field(field: &syn::Field) -> proc_macro2::TokenStream {
     let name = &field.ident;
     quote::quote! {
         #name: None
+    }
+}
+
+fn impl_builder(input: &syn::DeriveInput) -> proc_macro2::TokenStream {
+    let builder_name = builder_name(input);
+    let struct_fields = struct_fields(&input.data);
+    let setters = struct_fields.iter().map(|field| impl_builder_setter(field));
+    quote::quote! {
+        impl #builder_name {
+            #(#setters)*
+        }
+    }
+}
+
+fn impl_builder_setter(field: &syn::Field) -> proc_macro2::TokenStream {
+    let name = &field.ident;
+    let ty = &field.ty;
+    quote::quote! {
+        pub fn #name(&mut self, #name: #ty) -> &mut Self {
+            self.#name = Some(#name);
+            self
+        }
     }
 }
