42052b3b7a3ba9c67aebde408a8daa7f3cd1ec01
085astatine <astatine085@gmail.com>
2022-02-07 00:32:16 +0900

    seq: 05-repeat-section

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,21 +1,13 @@
+#![feature(step_trait)]
+
 #[proc_macro]
 pub fn seq(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let seq = syn::parse_macro_input!(input as Seq);
 
-    match seq2(seq) {
+    match seq.expand() {
         Ok(token_stream) => token_stream.into(),
         Err(error) => error.into_compile_error().into(),
     }
-}
-
-fn seq2(seq: Seq) -> syn::Result<proc_macro2::TokenStream> {
-    let mut streams = Vec::new();
-    for i in seq.range::<i64>()? {
-        streams.push(expand_token_stream(&seq.token_stream(), &seq.target, &i));
-    }
-    Ok(quote::quote! {
-        #(#streams)*
-    })
 }
 
 #[derive(Debug)]
@@ -26,6 +18,12 @@ struct Seq {
     _dot2_token: syn::Token![..],
     end: syn::LitInt,
     token_tree: proc_macro2::TokenTree,
+}
+
+#[derive(Debug, PartialEq)]
+enum SeqExpandType {
+    Site,
+    Section,
 }
 
 impl Seq {
@@ -44,6 +42,29 @@ impl Seq {
             group.stream()
         } else {
             proc_macro2::TokenStream::new()
+        }
+    }
+
+    fn expand_type(&self) -> SeqExpandType {
+        expand_type(self.token_stream())
+    }
+
+    fn expand(&self) -> syn::Result<proc_macro2::TokenStream> {
+        match self.expand_type() {
+            SeqExpandType::Site => {
+                let mut streams = Vec::new();
+                for i in self.range::<i64>()? {
+                    streams.push(expand_token_stream(self.token_stream(), &self.target, &i));
+                }
+                Ok(quote::quote! {
+                    #(#streams)*
+                })
+            }
+            SeqExpandType::Section => {
+                let range = self.range::<i64>()?;
+                let expanded = expand_repeat_section(self.token_stream(), &self.target, &range);
+                Ok(proc_macro2::TokenStream::from_iter(expanded))
+            }
         }
     }
 }
@@ -69,6 +90,23 @@ impl syn::parse::Parse for Seq {
     }
 }
 
+fn expand_type(stream: proc_macro2::TokenStream) -> SeqExpandType {
+    let mut iter = stream.into_iter();
+    while let Some(tree) = iter.next() {
+        // #(...)*
+        if extract_repeat_section(&tree, &mut iter).is_some() {
+            return SeqExpandType::Section;
+        }
+        // group
+        if let proc_macro2::TokenTree::Group(group) = tree {
+            if expand_type(group.stream()) == SeqExpandType::Section {
+                return SeqExpandType::Section;
+            }
+        }
+    }
+    SeqExpandType::Site
+}
+
 fn expand_token_tree<T: ToLiteral + std::fmt::Display>(
     tree: &proc_macro2::TokenTree,
     target: &syn::Ident,
@@ -81,7 +119,7 @@ fn expand_token_tree<T: ToLiteral + std::fmt::Display>(
         proc_macro2::TokenTree::Group(ref group) => {
             let mut expanded = proc_macro2::Group::new(
                 group.delimiter(),
-                expand_token_stream(&group.stream(), target, index),
+                expand_token_stream(group.stream(), target, index),
             );
             expanded.set_span(group.span());
             proc_macro2::TokenTree::Group(expanded)
@@ -91,7 +129,7 @@ fn expand_token_tree<T: ToLiteral + std::fmt::Display>(
 }
 
 fn expand_token_stream<T>(
-    input: &proc_macro2::TokenStream,
+    input: proc_macro2::TokenStream,
     target: &syn::Ident,
     index: &T,
 ) -> proc_macro2::TokenStream
@@ -123,9 +161,66 @@ where
             _ => expanded.push(expand_token_tree(&tree, target, index)),
         }
     }
-    let mut stream = proc_macro2::TokenStream::new();
-    stream.extend(expanded);
-    stream
+    proc_macro2::TokenStream::from_iter(expanded)
+}
+
+fn expand_repeat_section<T>(
+    stream: proc_macro2::TokenStream,
+    target: &proc_macro2::Ident,
+    range: &std::ops::Range<T>,
+) -> Vec<proc_macro2::TokenTree>
+where
+    T: ToLiteral + std::fmt::Display + std::iter::Step,
+{
+    let mut expanded = Vec::new();
+    let mut iter = stream.into_iter();
+    while let Some(tree) = iter.next() {
+        // section: #(...)*
+        if let Some(section) = extract_repeat_section(&tree, &mut iter) {
+            let range = range.clone();
+            for i in range {
+                expanded.extend(expand_token_stream(section.clone(), target, &i));
+            }
+        } else if let proc_macro2::TokenTree::Group(group) = tree {
+            let mut expanded_group = proc_macro2::Group::new(
+                group.delimiter(),
+                proc_macro2::TokenStream::from_iter(expand_repeat_section(
+                    group.stream(),
+                    target,
+                    range,
+                )),
+            );
+            expanded_group.set_span(group.span());
+            expanded.push(proc_macro2::TokenTree::Group(expanded_group));
+        } else {
+            expanded.push(tree);
+        }
+    }
+    expanded
+}
+
+fn extract_repeat_section(
+    tree: &proc_macro2::TokenTree,
+    iter: &mut <proc_macro2::TokenStream as IntoIterator>::IntoIter,
+) -> Option<proc_macro2::TokenStream> {
+    let mut peek = iter.clone();
+    if let (
+        proc_macro2::TokenTree::Punct(pound),
+        Some(proc_macro2::TokenTree::Group(group)),
+        Some(proc_macro2::TokenTree::Punct(star)),
+    ) = (tree, peek.next(), peek.next())
+    {
+        if pound.as_char() == '#'
+            && group.delimiter() == proc_macro2::Delimiter::Parenthesis
+            && star.as_char() == '*'
+        {
+            // move iterator: "(...)", "*"
+            iter.next();
+            iter.next();
+            return Some(group.stream());
+        }
+    }
+    None
 }
 
 trait ToLiteral {
